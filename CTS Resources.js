190b3958e3d0b288082aa48b39930833b040ae09
LASCAMPIA <contact@lascampia67.fr>
2026-08-10 01:38:27 +0200

    Remove duplicated embedded databases from resources

--- CTS Resources.js
+++ CTS Resources.js
@@ -1,623 +1,58 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: yellow; icon-glyph: magic;
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: yellow; icon-glyph: magic;
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: red; icon-glyph: shippingbox.fill;
+// icon-color: yellow; icon-glyph: shippingbox.fill;
 
 // CTS Resources.js
-// Installation automatique des ressources indispensables à CTS Dashboard.
+// Validation et réparation à la demande des bases CTS.
 
 const CONFIG = importModule("CTS Config")
+const STORAGE = importModule("CTS Storage")
 
 const {
   fm,
   files,
+  repository,
   ensureDirectories
 } = CONFIG
 
-const RESOURCES_VERSION = 3
+const RESOURCES_VERSION = 4
+const REQUEST_TIMEOUT_SECONDS = 20
 
-const DATABASES = {
-  lines: {
-    path: files.lines,
-    label: "lines.json",
-    content: {
-      "10": {
-        "name": "10"
-      },
-      "15": {
-        "name": "15"
-      },
-      "17": {
-        "name": "17"
-      },
-      "30": {
-        "name": "30"
-      },
-      "40": {
-        "name": "40"
-      },
-      "70": {
-        "name": "70"
-      },
-      "80": {
-        "name": "A"
-      },
-      "81": {
-        "name": "B"
-      },
-      "82": {
-        "name": "C"
-      },
-      "83": {
-        "name": "D"
-      },
-      "84": {
-        "name": "E"
-      },
-      "85": {
-        "name": "F"
-      },
-      "90": {
-        "name": "G"
-      },
-      "92": {
-        "name": "H"
-      },
-      "01": {
-        "name": "C1"
-      },
-      "02": {
-        "name": "2"
-      },
-      "03": {
-        "name": "C3"
-      },
-      "04": {
-        "name": "C4"
-      },
-      "05": {
-        "name": "C5"
-      },
-      "06": {
-        "name": "C6"
-      },
-      "07": {
-        "name": "C7"
-      },
-      "08": {
-        "name": "C8"
-      },
-      "09": {
-        "name": "C9"
-      }
-    }
+const DATABASES = Object.freeze([
+  {
+    name: "lines.json",
+    path: files.lines
   },
-
-  stops: {
-    path: files.stops,
-    label: "stops.json",
-    content: {
-      "SCHILTIGHEIM CAMPUS": {
-        "name": "Schiltigheim Campus"
-      },
-      "ILLKIRCH FORT UHRICH": {
-        "name": "Illkirch Fort Uhrich"
-      },
-      "ELMERFORST": {
-        "name": "Elmerforst"
-      },
-      "JARDIN DES DEUX RIVES": {
-        "name": "Jardin des Deux Rives"
-      },
-      "LINGOLSHEIM GARE": {
-        "name": "Lingolsheim Gare"
-      },
-      "MONTAGNE VERTE": {
-        "name": "Montagne Verte",
-        "aliases": [
-          "MOVE_A",
-          "MOVE_C"
-        ]
-      },
-      "NEUHOF GANZAU": {
-        "name": "Neuhof Ganzau"
-      },
-      "UNTERELSAU": {
-        "name": "Unterelsau"
-      },
-      "GARE CENTRALE": {
-        "name": "Gare Centrale"
-      },
-      "ELSAU": {
-        "name": "Elsau"
-      },
-      "ELSAU CO V2": {
-        "name": "Elsau Co V2"
-      },
-      "PARLEMENT EUROPEEN": {
-        "name": "Parlement Européen"
-      },
-      "PLACE D'ISLANDE": {
-        "name": "Place d’Islande"
-      },
-      "OSTWALD HOTEL DE VILLE": {
-        "name": "Ostwald Hôtel de Ville"
-      },
-      "PLACE D'OSTWALD": {
-        "name": "Place d’Ostwald"
-      },
-      "LYCEE KLEBER": {
-        "name": "Lycée Kléber"
-      },
-      "LYCEE COUFFIGNAL": {
-        "name": "Lycée Couffignal"
-      },
-      "NEUHOF RODOLPHE REUSS": {
-        "name": "Neuhof Rodolphe Reuss",
-        "aliases": [
-          "RODOLPHE REUSS"
-        ]
-      },
-      "CITE MEINAU": {
-        "name": "Cité Meinau"
-      },
-      "BAGGERSEE": {
-        "name": "Baggersee"
-      },
-      "NEUHOF LUCIE AUBRAC": {
-        "name": "Neuhof Lucie Aubrac"
-      },
-      "NEUHOF CORPS EUROPEENS": {
-        "name": "Neuhof Corps Européens"
-      },
-      "LORIENT": {
-        "name": "Lorient"
-      },
-      "LA ROCHELLE": {
-        "name": "La Rochelle"
-      },
-      "SAINT NAZAIRE": {
-        "name": "Saint-Nazaire"
-      },
-      "PORT AUTONOME SUD": {
-        "name": "Port Autonome Sud"
-      },
-      "ILLKIRCH MAIRIE": {
-        "name": "Illkirch Mairie",
-        "aliases": [
-          "ILLKIRCH MAIRIE DESCENTE",
-          "ILLKIRCH MAIRIE MONTEE"
-        ]
-      },
-      "ROME": {
-        "name": "Rome"
-      },
-      "ESPLANADE": {
-        "name": "Esplanade",
-        "aliases": [
-          "ESPLANADE DESCENTE"
-        ]
-      },
-      "LONDRES": {
-        "name": "Londres"
-      },
-      "IUT PASTEUR": {
-        "name": "IUT Pasteur"
-      },
-      "PARC DES SPORTS": {
-        "name": "Parc des Sports"
-      },
-      "BUGATTI": {
-        "name": "Bugatti"
-      },
-      "POTERIES": {
-        "name": "Poteries"
-      },
-      "OCTROI": {
-        "name": "Octroi"
-      },
-      "BOEUF ROUGE": {
-        "name": "Bœuf Rouge"
-      },
-      "WOLFISHEIM STADE TERMINI": {
-        "name": "Wolfisheim Stade",
-        "aliases": [
-          "WOLFISHEIM STADE",
-          "WOLFISHEIM STADE TERMINUS"
-        ]
-      },
-      "ROBERTSAU RENAISSANCE": {
-        "name": "Robertsau Renaissance"
-      },
-      "PONT PHARIO": {
-        "name": "Pont Phario"
-      },
-      "MAIRES SCHAUB": {
-        "name": "Maires Schaub"
-      },
-      "BISCHHEIM GARE": {
-        "name": "Bischheim Gare"
-      },
-      "CHAMBRE DE METIERS": {
-        "name": "Chambre de Métiers",
-        "aliases": [
-          "CHAMBRE DE METIERS ARRIVEE",
-          "CHAMBRE DE METIERS ARRIVEE/DEPART",
-          "CHAMBRE DE METIERS ARRI"
-        ]
-      },
-      "HOENHEIM BATTEMENT": {
-        "name": "Hœnheim Battement"
-      },
-      "HOENHEIM GARE": {
-        "name": "Hœnheim Gare"
-      },
-      "HOENHEIM GARE TIROIR": {
-        "name": "Hœnheim Gare Tiroir"
-      },
-      "DAUPHINE": {
-        "name": "Dauphine"
-      },
-      "FORT DESAIX": {
-        "name": "Fort Desaix"
-      },
-      "VICTOR HUGO": {
-        "name": "Victor Hugo"
-      },
-      "POINCARE": {
-        "name": "Poincaré"
-      },
-      "SAINTE HELENE": {
-        "name": "Sainte-Hélène"
-      },
-      "WILSON": {
-        "name": "Wilson"
-      },
-      "HALLES PONT DE PARIS": {
-        "name": "Halles Pont de Paris",
-        "aliases": [
-          "LES HALLES PONT DE PARIS"
-        ]
-      },
-      "REPUBLIQUE": {
-        "name": "République"
-      },
-      "ORANGERIE": {
-        "name": "Orangerie"
-      },
-      "ROBERTSAU EGLISE": {
-        "name": "Robertsau Église"
-      },
-      "CITE DE L'ILL": {
-        "name": "Cité de l’Ill"
-      },
-      "GARE AUX MARCHANDISES": {
-        "name": "Gare aux Marchandises"
-      },
-      "PORTE BLANCHE": {
-        "name": "Porte Blanche"
-      },
-      "LAITERIE": {
-        "name": "Laiterie"
-      },
-      "HOPITAL CIVIL": {
-        "name": "Hôpital Civil"
-      },
-      "ETOILE BOURSE": {
-        "name": "Étoile Bourse"
-      },
-      "WINSTON CHURCHILL": {
-        "name": "Winston Churchill"
-      },
-      "DANUBE LE VAISSEAU": {
-        "name": "Danube Le Vaisseau"
-      },
-      "ROTTERDAM": {
-        "name": "Rotterdam"
-      },
-      "RIETH": {
-        "name": "Rieth"
-      },
-      "ARAGO": {
-        "name": "Arago"
-      },
-      "ESPACE EUROPEEN DE L'ENT": {
-        "name": "Espace Européen de l’Entreprise",
-        "aliases": [
-          "ESPACE EUROPEEN DE L'ENTREPRISE"
-        ]
-      },
-      "PLACE DE PIERRE": {
-        "name": "Place de Pierre"
-      },
-      "BARR": {
-        "name": "Barr"
-      },
-      "MAISON ROUGE": {
-        "name": "Maison Rouge"
-      },
-      "PARC WODLI": {
-        "name": "Parc Wodli"
-      },
-      "DEPOT ELSAU": {
-        "name": "Dépôt Elsau"
-      },
-      "DEPOT KIBITZENAU": {
-        "name": "Dépôt Kibitzenau"
-      },
-      "DEPOT CRONENBOURG": {
-        "name": "Dépôt Cronenbourg"
-      },
-      "ROTONDE": {
-        "name": "Rotonde"
-      },
-      "ROTONDE TIROIR ARRIVEE": {
-        "name": "Rotonde Tiroir"
-      },
-      "ROTONDE TIROIR DEPART": {
-        "name": "Rotonde Tiroir"
-      },
-      "DANTE": {
-        "name": "Dante"
-      },
-      "HOMME DE FER": {
-        "name": "Homme de Fer"
-      },
-      "HOMME DE FER Q1 ARRIVEE": {
-        "name": "Homme de Fer Q1"
-      },
-      "HOMME DE FER Q2 ARRIVEE": {
-        "name": "Homme de Fer Q2"
-      },
-      "HOMME DE FER V.1 ARRIVEE": {
-        "name": "Homme de Fer V1"
-      },
-      "HOMME DE FER V.2 ARRIVEE": {
-        "name": "Homme de Fer V2"
-      },
-      "ETOILE POLYGONE": {
-        "name": "Étoile Polygone"
-      },
-      "LANDSBERG": {
-        "name": "Landsberg"
-      },
-      "JEAN JAURES": {
-        "name": "Jean Jaurès"
-      },
-      "ARISTIDE BRIAND": {
-        "name": "Aristide Briand"
-      },
-      "PORT DU RHIN": {
-        "name": "Port du Rhin"
-      },
-      "KEHL BAHNHOF": {
-        "name": "Kehl Bahnhof"
-      },
-      "HOCHSCHULE / LAGER": {
-        "name": "Hochschule / Lager"
-      },
-      "KEHL RATHAUS": {
-        "name": "Kehl Rathaus"
-      },
-      "KRIMMERI - ST. MEINAU": {
-        "name": "Krimmeri - St. Meinau"
-      },
-      "KRIMMERI - ST. MEINAU Q1 ARRIVEE": {
-        "name": "Krimmeri - St. Meinau Q1"
-      },
-      "KRIMMERI - ST. MEINAU Q2 ARRIVEE": {
-        "name": "Krimmeri - St. Meinau Q2"
-      },
-      "CAMPUS D'ILLKIRCH": {
-        "name": "Campus d’Illkirch"
-      },
-      "TIOIR CAMPUS ILLKIRCH DEPART": {
-        "name": "Tiroir Campus d’Illkirch"
-      },
-      "ILLKIRCH LIXENBUHL": {
-        "name": "Illkirch Lixenbuhl"
-      },
-      "PARC MALRAUX": {
-        "name": "Parc Malraux"
-      },
-      "COURS DE L'ILLIADE": {
-        "name": "Cours de l’Illiade"
-      },
-      "ILLKIRCH GRAFFENSTADEN": {
-        "name": "Illkirch Graffenstaden"
-      },
-      "OBSERVATOIRE": {
-        "name": "Observatoire"
-      },
-      "WACKEN": {
-        "name": "Wacken"
-      },
-      "BOECKLIN": {
-        "name": "Boecklin"
-      },
-      "ROBERTSAU L'ESCALE": {
-        "name": "Robertsau l’Escale"
-      },
-      "GRAVIERE": {
-        "name": "Gravière"
-      },
-      "BROGLIE": {
-        "name": "Broglie"
-      },
-      "FAUBOURG NATIONAL": {
-        "name": "Faubourg National"
-      },
-      "PARC DES ROMAINS": {
-        "name": "Parc des Romains"
-      },
-      "COMTES": {
-        "name": "Comtes"
-      },
-      "GRUBER": {
-        "name": "Gruber"
-      },
-      "WOLFISHEIM HENRI RENDU": {
-        "name": "Wolfisheim Henri Rendu"
-      },
-      "MARTIN SCHONGAUER": {
-        "name": "Martin Schongauer"
-      },
-      "LINGOLSHEIM TIERGAERTEL": {
-        "name": "Lingolsheim Tiergaertel"
-      }
-    }
+  {
+    name: "stops.json",
+    path: files.stops
   },
-
-  places: {
-    path: files.places,
-    label: "places.json",
-    content: {
-      "ELS": {
-        "name": "UPE",
-        "type": "depot"
-      },
-      "KBZ": {
-        "name": "UPK",
-        "type": "depot"
-      },
-      "CRB": {
-        "name": "UPC",
-        "type": "depot"
-      },
-      "ELSA": {
-        "name": "Elsau",
-        "type": "relief"
-      },
-      "ELSA_1": {
-        "name": "Elsau",
-        "type": "relief"
-      },
-      "ELSA_2": {
-        "name": "Elsau",
-        "type": "relief"
-      },
-      "ELME": {
-        "name": "Elmerforst",
-        "type": "relief"
-      },
-      "ELME_A": {
-        "name": "Elmerforst",
-        "type": "relief"
-      },
-      "MOVE": {
-        "name": "Montagne Verte",
-        "type": "relief"
-      },
-      "MOVE_A": {
-        "name": "Montagne Verte",
-        "type": "relief"
-      },
-      "MOVE_C": {
-        "name": "Montagne Verte",
-        "type": "relief"
-      },
-      "GACE": {
-        "name": "Gare Centrale",
-        "type": "relief"
-      },
-      "REPU": {
-        "name": "République",
-        "type": "relief"
-      },
-      "HOHB": {
-        "name": "Hohberg",
-        "type": "relief"
-      },
-      "HOHB_A": {
-        "name": "Hohberg",
-        "type": "relief"
-      },
-      "RORE": {
-        "name": "Neuhof Rodolphe Reuss",
-        "type": "relief"
-      },
-      "RORE_A": {
-        "name": "Neuhof Rodolphe Reuss",
-        "type": "relief"
-      },
-      "ROMEEE": {
-        "name": "Rome",
-        "type": "relief"
-      },
-      "ESPL_A": {
-        "name": "Esplanade",
-        "type": "relief"
-      },
-      "ESPL_1": {
-        "name": "Esplanade",
-        "type": "relief"
-      },
-      "ESPL_2": {
-        "name": "Esplanade",
-        "type": "relief"
-      },
-      "OBSE_1": {
-        "name": "Observatoire",
-        "type": "relief"
-      },
-      "ROTO_1": {
-        "name": "Rotonde",
-        "type": "relief"
-      },
-      "ROTO_2": {
-        "name": "Rotonde",
-        "type": "relief"
-      },
-      "FANA_1": {
-        "name": "Faubourg National",
-        "type": "relief"
-      },
-      "LOND_B": {
-        "name": "Londres",
-        "type": "relief"
-      },
-      "GAMA_A": {
-        "name": "Gare aux Marchandises",
-        "type": "relief"
-      },
-      "LHPP_G": {
-        "name": "Halles Pont de Paris",
-        "type": "relief"
-      }
-    }
+  {
+    name: "places.json",
+    path: files.places
   }
-}
+])
 
 async function ensureInstalled() {
   ensureDirectories()
 
   const installed = []
-  const preserved = []
   const repaired = []
+  const preserved = []
 
-  for (
-    const resource
-    of Object.values(DATABASES)
-  ) {
-    const status =
-      await ensureDatabase(resource)
+  for (const resource of DATABASES) {
+    const status = await ensureDatabase(resource)
 
     if (status === "installed") {
-      installed.push(resource.label)
+      installed.push(resource.name)
     } else if (status === "repaired") {
-      repaired.push(resource.label)
+      repaired.push(resource.name)
     } else {
-      preserved.push(resource.label)
+      preserved.push(resource.name)
     }
   }
 
   return {
-    success: true,
     version: RESOURCES_VERSION,
     installed,
     repaired,
@@ -626,78 +61,119 @@ async function ensureInstalled() {
 }
 
 async function ensureDatabase(resource) {
-  if (!fm.fileExists(resource.path)) {
-    writeDatabase(resource)
-    return "installed"
+  const existed = fm.fileExists(resource.path)
+
+  if (await isValidDatabase(resource.path)) {
+    return "preserved"
   }
 
+  const content = await downloadDatabase(resource.name)
+  const parsed = parseDatabase(content, resource.name)
+
+  await STORAGE.writeTextSafely(
+    resource.path,
+    JSON.stringify(parsed, null, 2)
+  )
+
+  if (!await isValidDatabase(resource.path)) {
+    throw new Error(
+      `${resource.name} reste invalide après réparation.`
+    )
+  }
+
+  return existed ? "repaired" : "installed"
+}
+
+async function isValidDatabase(path) {
   try {
-    if (!fm.isFileDownloaded(resource.path)) {
-      await fm.downloadFileFromiCloud(
-        resource.path
-      )
+    if (!await STORAGE.ensureDownloaded(path)) {
+      return false
     }
 
-    const content =
-      fm.readString(resource.path).trim()
+    const content = fm.readString(path).trim()
 
     if (!content) {
-      writeDatabase(resource)
-      return "repaired"
+      return false
     }
 
-    const parsed =
-      JSON.parse(content)
+    const value = JSON.parse(content)
 
-    if (!isPlainObject(parsed)) {
-      writeDatabase(resource)
-      return "repaired"
-    }
-
-    return "preserved"
-  } catch (error) {
-    writeDatabase(resource)
-    return "repaired"
+    return Boolean(
+      value &&
+      typeof value === "object" &&
+      !Array.isArray(value) &&
+      Object.keys(value).length
+    )
+  } catch (_) {
+    return false
   }
 }
 
-function writeDatabase(resource) {
-  const temporaryPath =
-    `${resource.path}.installation`
+async function downloadDatabase(name) {
+  const request = new Request(rawUrl(name))
+  request.timeoutInterval = REQUEST_TIMEOUT_SECONDS
+  request.headers = {
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache"
+  }
 
-  removeQuietly(temporaryPath)
+  let content
 
-  fm.writeString(
-    temporaryPath,
-    JSON.stringify(
-      resource.content,
-      null,
-      2
-    )
-  )
-
-  removeQuietly(resource.path)
-
-  fm.move(
-    temporaryPath,
-    resource.path
-  )
-}
-
-function isPlainObject(value) {
-  return Boolean(
-    value &&
-    typeof value === "object" &&
-    !Array.isArray(value)
-  )
-}
-
-function removeQuietly(path) {
   try {
-    if (fm.fileExists(path)) {
-      fm.remove(path)
+    content = await request.loadString()
+  } catch (error) {
+    throw new Error(
+      `${name} impossible à télécharger : ${messageOf(error)}`
+    )
+  }
+
+  const statusCode = Number(request.response?.statusCode)
+
+  if (
+    Number.isFinite(statusCode) &&
+    (statusCode < 200 || statusCode >= 300)
+  ) {
+    throw new Error(
+      `${name} impossible à télécharger : HTTP ${statusCode}`
+    )
+  }
+
+  return content
+}
+
+function parseDatabase(content, name) {
+  try {
+    const value = JSON.parse(String(content || ""))
+
+    if (
+      !value ||
+      typeof value !== "object" ||
+      Array.isArray(value) ||
+      !Object.keys(value).length
+    ) {
+      throw new Error("racine JSON invalide")
     }
-  } catch (error) {}
+
+    return value
+  } catch (error) {
+    throw new Error(
+      `${name} invalide : ${messageOf(error)}`
+    )
+  }
+}
+
+function rawUrl(name) {
+  return [
+    "https://raw.githubusercontent.com",
+    encodeURIComponent(repository.owner),
+    encodeURIComponent(repository.name),
+    encodeURIComponent(repository.branch),
+    encodeURIComponent(name)
+  ].join("/") + `?t=${Date.now()}`
+}
+
+function messageOf(error) {
+  return error?.message?.trim?.() || String(error || "Erreur inconnue")
 }
 
 module.exports = {
